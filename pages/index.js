d1639d0f8fc3b42bec060a0ca08579b5c5bba146
Michel Silva <micheljanura@hotmail.com>
2023-07-07 15:26:50 -0300

    add request

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 function HomePage() {
+  fetch(
+    'https://posgolivemichel--nexodata.myvtex.com/_v/my-cart/changePrice'
+  ).then((response) => console.log(response))
   return (
     <div>
       <h1>The Home Page</h1>
     </div>
-  );
+  )
 }
 
-export default HomePage;
+export default HomePage
